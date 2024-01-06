246021af6adec0d44442afba8ccdf324099c2de0
Gladwell Kinyanjui (Techno Brain Global FZ LLC) <v-gkinyanjui@microsoft.com>
2024-01-07 00:59:22 +0300

    Create an API for movies

---
+++ MovieAPI/Data/MovieDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Models;
+
+namespace MovieAPI.Data
+{
+    public class MovieDBContext : DbContext
+    {
+        public MovieDBContext(DbContextOptions<MovieDBContext> options) : base(options) 
+        { 
+
+        }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
---
+++ MovieAPI/Models/Movies.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieAPI.Models
+{
+    public class Movies
+    {
+        [Key]
+        public Guid Guid = new Guid();
+
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+
+        
+    }
+    public enum Genre
+    {
+        Romantic,
+        Comedy,
+        Action,
+        Mystery,
+        Horror
+    }
+}
